fdcf40fd7171eb1f4b4b12b6d27636f84663a03b
Zombiegoblin4 <astridnijhof4@gmail.com>
2026-07-03 10:18:29 +0200

    Add conditional loading for macobby script

--- loader.lua
+++ loader.lua
@@ -4,4 +4,6 @@ local universeId = game.GameId
 
 if placeId == 72165087146441 or universeId == 72165087146441 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zombiegoblin4/TheCoolestHubRBLX/refs/heads/main/games/Easy-Fun-Obby-575-Stages.lua"))()
+elseif placeId == 17732590459 or universeId == 17732590459 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Zombiegoblin4/TheCoolestHubRBLX/refs/heads/main/games/macobby.lua"))()
 end
