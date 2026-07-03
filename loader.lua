08311d3deb782aed9897de36190283f58fe44713
Zombiegoblin4 <astridnijhof4@gmail.com>
2026-07-03 09:26:22 +0200

    Add loader for Easy Fun Obby game

---
+++ loader.lua
@@ -0,0 +1,7 @@
+-- 1: 72165087146441
+local placeId = game.PlaceId
+local universeId = game.GameId
+
+if placeId == 72165087146441 or universeId == 72165087146441 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Zombiegoblin4/TheCoolestHubRBLX/refs/heads/main/games/Easy-Fun-Obby-575-Stages.lua"))()
+end
