cb69004e31c552dc2827d6df61c1a309da9a2b8e
Zombiegoblin4 <astridnijhof4@gmail.com>
2026-07-03 10:20:10 +0200

    Add new game loader for housetycoon1

--- loader.lua
+++ loader.lua
@@ -6,4 +6,6 @@ if placeId == 72165087146441 or universeId == 72165087146441 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zombiegoblin4/TheCoolestHubRBLX/refs/heads/main/games/Easy-Fun-Obby-575-Stages.lua"))()
 elseif placeId == 17732590459 or universeId == 17732590459 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zombiegoblin4/TheCoolestHubRBLX/refs/heads/main/games/macobby.lua"))()
+elseif placeId == 3571215756 or universeId == 3571215756 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Zombiegoblin4/TheCoolestHubRBLX/refs/heads/main/games/housetycoon1.lua"))()
 end
